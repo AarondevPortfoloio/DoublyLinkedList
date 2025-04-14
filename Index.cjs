9aa2de13c41e0b8b9663d66a7b5992262bfaa2f1
Aaron Dixson <88754367+AarondevPortfoloio@users.noreply.github.com>
2025-04-14 15:58:23 -0400

    Initial commit

---
+++ Index.cjs
@@ -0,0 +1,20 @@
+const Node = require('./Node.cjs');
+const DoublyLinkedList = require('./DoublyLinkedList.cjs')
+
+const subway = new DoublyLinkedList();
+
+subway.addToHead('TimesSquare');
+subway.addToHead('GrandCentral');
+subway.addToHead('CentralPark');
+
+subway.printList();
+subway.addToTail('PennStation');
+subway.addToTail('WallStreet');
+subway.addToTail('BrooklynBridge');
+
+subway.printList();
+subway.removeHead();
+subway.removeTail();
+
+subway.removeByData('TimesSquare');
+subway.printList();
